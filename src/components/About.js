686030568ee8c3f6b09138122ad3d2fa1b7f89b6
jnanendranagara <jvijayakumarostb1.com>
2019-08-19 20:24:00 -0700

    about us page completed

--- src/components/About.js
+++ src/components/About.js
@@ -21,11 +21,11 @@ class About extends Component {
               <div className="header">
                  <Navbar/>
               </div>
-           
+       <div className="row">
+             <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+          <div className="center"> 
               <div className="row">
-             
-               
-              <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+              <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
                  <Card className='img_crd'>
                     <CardImg top width="100%" height='150px' src={santhosh} alt="santhosh" />
                     <CardBody className="cardbody">
@@ -34,7 +34,7 @@ class About extends Component {
                     </CardBody>
                   </Card>
               </div>
-              <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+              <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
                 <Card className="img_crd">
                 <CardImg top width="100%" height='150px' src={ganguly} alt="ganguly" />
                   <CardBody className="cardbody">
@@ -43,7 +43,7 @@ class About extends Component {
                   </CardBody>                 
                 </Card>
               </div>
-              <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+              <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
                 <Card className="img_crd">
                   <CardImg top width="100%" height='150px' src={dharshan} alt="dharshan" />
                     <CardBody className="cardbody">
@@ -52,31 +52,18 @@ class About extends Component {
                     </CardBody>                 
                   </Card>
               </div>
-              <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+              <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
                   <Card className="img_crd">
                       <CardImg top width="100%" height='150px' src={maria} alt="maria" />
                         <CardBody className="cardbody">
                         <CardTitle className='card_ttl' >Maria Sharapova</CardTitle>
-                        <CardText className='designation'><b>Secretory</b> </CardText>
+                        <CardText className='designation'><b>Director</b> </CardText>
                         </CardBody>                 
                      </Card>
               </div>
-              <div id="aboutcollage" className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                    {/* <p > Sri K.S Puttannaiah, fondly called as KSP, 
-                    former minister of education and well known as "Raitha Nayaka" founded 
-                    and registered J.K Institution currently known as JK Institute(R) in 1958.</p>
-                    <p>1) JK College of Science, Arts & Commerce, Pandavapura	<br/> 
-                      2)   JK Law College,Pandavapura<br/> 
-                      3)JK College of Education, Pandavapura<br/> 
-                      4)	    JK Evening College, Pandavapura<br/> 
-                      5) JK PU College, Pandavapura	 <br/> 
-                      6)   JK High School, Pandavapura<br/> 
-                      7) JK Post Graduate Center for Education and Research [M.Ed], Pandavapura	 <br/>  
-                      8) Krishik Sarvodaya Foundation JK Competitive Exam Training Center, Pandavapura</p> */}
-                      </div> 
               </div>
               <div className="row">
-              <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+              <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
                  <Card className='img_crd'>
                     <CardImg top width="100%" height='150px' src={hrithik} alt="hrithik" />
                     <CardBody className="cardbody">
@@ -85,7 +72,7 @@ class About extends Component {
                     </CardBody>
                   </Card>
               </div>
-              <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+              <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
                  <Card className='img_crd'>
                     <CardImg top width="100%" height='150px' src={katrina} alt="katrina" />
                     <CardBody className="cardbody">
@@ -94,7 +81,7 @@ class About extends Component {
                     </CardBody>
                   </Card>
               </div>
-              <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+              <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
                  <Card className='img_crd'>
                     <CardImg top width="100%" height='150px' src={punith} alt="punith" />
                     <CardBody className="cardbody">
@@ -103,7 +90,7 @@ class About extends Component {
                     </CardBody>
                   </Card>
               </div>
-              <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+              <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
                  <Card className='img_crd'>
                     <CardImg top width="100%" height='150px' src={anna} alt="anna" />
                     <CardBody className="cardbody">
@@ -111,12 +98,24 @@ class About extends Component {
                     <CardText className='designation'><b>Member</b> </CardText>
                     </CardBody>
                   </Card>
-              </div>               
-            
-
-
+              </div>              
               </div>
-              
+          </div>
+          </div>
+              <div id="element1" className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                        <p > Sri K.S Puttannaiah, fondly called as KSP, 
+                        former minister of education and well known as "Raitha Nayaka" founded 
+                        and registered J.K Institution currently known as JK Institute(R) in 1958.</p>
+                        <p>1) JK College of Science, Arts & Commerce, Pandavapura	<br/> 
+                          2)   JK Law College,Pandavapura<br/> 
+                          3)JK College of Education, Pandavapura<br/> 
+                          4)	    JK Evening College, Pandavapura<br/> 
+                          5) JK PU College, Pandavapura	 <br/> 
+                          6)   JK High School, Pandavapura<br/> 
+                          7) JK Post Graduate Center for Education and Research [M.Ed], Pandavapura	 <br/>  
+                          8) Krishik Sarvodaya Foundation JK Competitive Exam Training Center, Pandavapura</p> 
+                </div>
+          </div>      
               <div className="footer">
                 <Footer/>
               </div> 
